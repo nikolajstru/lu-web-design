7ceeed1a2929a89df2cb0581eaba83c83a7ec82d
nikolajstru <nikolajs.trubacistovs@delfi.lv>
2021-05-30 17:28:11 +0300

    Fix router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/lu-web-design/'
+    base: '/dist/lu-web-design/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
